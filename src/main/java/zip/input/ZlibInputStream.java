f948e5cb8f6bd387826c92a870e69b7532ff581c
stafflute <michaelius7@gmail.com>
2013-12-19 17:51:10 +0100

    refactoring e creazione dell'inputstream

---
+++ src/main/java/zip/input/ZlibInputStream.java
@@ -0,0 +1,14 @@
+package zip.input;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Input stream utilizzato per la decompressione
+ */
+public class ZlibInputStream extends InputStream {
+    @Override
+    public int read() throws IOException {
+        return 0;  //TODO
+    }
+}
---
+++ src/main/java/zip/output/ZlibOutputStream.java
@@ -0,0 +1,14 @@
+package zip.output;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Output stream utilizzato per la compressione
+ */
+public class ZlibOutputStream extends OutputStream {
+    @Override
+    public void write(int b) throws IOException {
+        //TODO
+    }
+}
