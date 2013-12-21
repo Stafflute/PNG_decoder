00bd00bd40f400d5374f0b78f356d1a035039e84
stafflute <michaelius7@gmail.com>
2013-12-21 10:41:16 +0100

    piccole modifiche

--- src/main/java/zip/input/ZlibInputStream.java
+++ src/main/java/zip/input/ZlibInputStream.java
@@ -1,8 +1,9 @@
 package zip.input;
 
+import util.BitInputStream;
 import util.ByteBitInputStream;
+import zip.deflate.Decompressor;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import static zip.structure.ZlibHeaderStructure.*;
@@ -10,18 +11,40 @@ import static zip.structure.ZlibHeaderStructure.*;
 /**
  * Input stream utilizzato per la decompressione
  */
-public class ZlibInputStream extends ByteBitInputStream {
+public class ZlibInputStream {
 
-    public ZlibInputStream(InputStream inputStream) {
-        super(inputStream);
+    private final BitInputStream bitInputStream;
+
+    public final int cinfo;
+    public final int cm;
+
+    public final int flevel;
+    public final int fdict;
+    public final int fcheck;
+
+    public ZlibInputStream(InputStream inputStream) throws IOException {
+         bitInputStream = new ByteBitInputStream(inputStream);
+
+        int size;
+
+        size = getFieldBitSize(CINFO);
+        cinfo =  bitInputStream.read(size);
+
+        size = getFieldBitSize(CM);
+        cm =  bitInputStream.read(size);
+
+        size = getFieldBitSize(FLEVEL);
+        flevel =  bitInputStream.read(size);
+
+        size = getFieldBitSize(FDICT);
+        fdict =  bitInputStream.read(size);
+
+        size = getFieldBitSize(FCHECK);
+        fcheck =  bitInputStream.read(size);
+
     }
 
-    private int index = 0;
-
-    public int readZlib() throws IOException {
-        int bitToGet = getFieldBitSize(index);
-        index++;
-        //TODO passing to DEFLATE
-        return read(bitToGet);
+    public byte[] readAll() throws IOException {
+        return Decompressor.decompress(bitInputStream);
     }
 }
