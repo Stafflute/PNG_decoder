de6f4f3c635189a20ebd28e8a698242103519540
stafflute <michaelius7@gmail.com>
2013-12-30 11:53:24 +0100

    creazione del chunk PLTE

---
+++ src/main/java/engine/chunk/specific/PLTEChunk.java
@@ -0,0 +1,20 @@
+package engine.chunk.specific;
+
+import engine.chunk.Chunk;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static util.ByteConverter.*;
+
+public class PLTEChunk extends Chunk{
+    @Override
+    protected void setContent() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    protected void readContent(InputStream inputStream) throws IOException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
