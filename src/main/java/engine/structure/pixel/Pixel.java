0e5b3f9731370208dddfffc5feafe6b579c5c9a7
stafflute <michaelius7@gmail.com>
2014-01-01 00:18:15 +0100

    Strutturazione dell'immagine

--- src/main/java/engine/structure/pixel/Pixel.java
+++ src/main/java/engine/structure/pixel/Pixel.java
@@ -1,7 +1,20 @@
 package engine.structure.pixel;
 
-public interface Pixel {
-    public byte[] getCode();
-    public int getByteSize();
-    public int getByte(int channel);
+import engine.structure.pixel.format.PixelFormat;
+
+public abstract class Pixel {
+    protected byte[] source;
+    protected PixelFormat format;
+
+    public byte[] getCode() {
+        return source;
+    }
+
+    public int getByteSize() {
+        return format.getByteSize();
+    }
+
+    public int getByte(int position) {
+        return source[position];
+    }
 }
