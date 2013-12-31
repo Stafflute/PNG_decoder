5b664021a05517ae25071851316198886bbdc590
stafflute <michaelius7@gmail.com>
2013-12-31 23:52:40 +0100

    Strutturazione dell'immagine

--- src/main/java/engine/structure/pixel/format/ColorType.java
+++ src/main/java/engine/structure/pixel/format/ColorType.java
@@ -4,17 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ColorType {
-    GREYSCALE (0),
-    TRUECOLOR (2),
-    PALETTE (3),
-    GREYSCALE_ALPHA (4),
-    TRUECOLOR_ALPHA (6),
-    UNKNOWN (-1);
+    GREYSCALE (0, 1),
+    TRUECOLOR (2, 3),
+    PALETTE (3, 1),
+    GREYSCALE_ALPHA (4, 2),
+    TRUECOLOR_ALPHA (6, 4),
+    UNKNOWN (-1, 0);
 
     private final int byteValue;
+    public final int byteSize;
 
-    private ColorType(int byteValue) {
+    private ColorType(int byteValue, int byteSize) {
         this.byteValue = byteValue;
+        this.byteSize = byteSize;
     }
 
     private static final Map<Integer, ColorType> COLOR_TYPE_MAP = new HashMap<>();
--- src/main/java/engine/structure/pixel/format/PixelFormat.java
+++ src/main/java/engine/structure/pixel/format/PixelFormat.java
@@ -6,13 +6,25 @@ public class PixelFormat {
     public final ColorType colorType;
     public final int depth;
 
+    private final int depthByteSize;
+
+    private final static int BIT16 = 16;
+    private final static int BIT16_SIZE_MULTIPLIER = 2;
+
+    private final static int BIT8_SIZE_MULTIPLIER = 1;
+
     public PixelFormat(int colorTypeByte, int depth) {
         this.colorType = getColorType(colorTypeByte);
         this.depth = depth;
+
+        if (depth == BIT16) {
+            depthByteSize = BIT16_SIZE_MULTIPLIER;
+        } else {
+            depthByteSize = BIT8_SIZE_MULTIPLIER;
+        }
     }
 
     public int getByteSize() {
-        //TODO
-        return 0;
+        return colorType.byteSize * depthByteSize;
     }
 }
